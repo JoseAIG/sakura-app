36257ef3e10451b93b223968762a7dd8e81cae7a
JoseAIG <inciartegj@gmail.com>
2021-11-06 17:32:32 -0400

    Removed local array for testing

--- src/app/pages/viewer/viewer.page.ts
+++ src/app/pages/viewer/viewer.page.ts
@@ -15,16 +15,7 @@ export class ViewerPage implements OnInit {
   mangaID: number
   chapterNumber: number
   title: string
-  // images: string[]
-
-  //https://mangastack.cf/manga/a96676e5-8ae2-425e-b549-7f15dd34a6d8/chapter/46b6501a-4c9e-4d91-974b-9274fe684c94
-  images: string[] = [
-    'https://uploads.mangadex.org/data/46933c48657c37e93b2e20ee7134085e/x1-6b3a1813f068bc2e7907b2747c385b9d1e5b97e1fb08d02f1e92aeff67ea582f.png',
-    'https://uploads.mangadex.org/data/46933c48657c37e93b2e20ee7134085e/x2-de4d2b348725247c3ca652a8726bc782ece37a49b94f5fdf08cdeeae6c791b20.png',
-    'https://uploads.mangadex.org/data/46933c48657c37e93b2e20ee7134085e/x3-f17dd2f1d98448df8ec48c85f60b2bf2ab0c014f9008f39fb050199c1fc6289c.png',
-    'https://uploads.mangadex.org/data/46933c48657c37e93b2e20ee7134085e/x3-f17dd2f1d98448df8ec48c85f60b2bf2ab0c014f9008f39fb050199c1fc6289c.png',
-    'https://uploads.mangadex.org/data/46933c48657c37e93b2e20ee7134085e/x4-878bad4cb82788231377eb591ff93793295153626339f6c9900e9658ff02c585.png'
-  ]
+  images: string[]
 
   private readMode: string = localStorage.getItem("READ_MODE")
   showToolbar = true;
@@ -51,32 +42,27 @@ export class ViewerPage implements OnInit {
       });
 
     // FETCH CHAPTER DATA
-    // this.http.get(`https://sakura-mv.herokuapp.com/manga/${this.mangaID}/chapter/${this.chapterNumber}`)
-    //   .subscribe(
-    //     (res: any) => {
-    //       console.log(res)
-    //       this.images = res.chapter_images
-    //       // REVERSE ARRAY IF READ MODE IS FROM BOTTOM TO TOP
-    //       if (this.readMode === "bottomToTop") {
-    //         this.images.reverse()
-    //       }
-    //     },
-    //     async (res) => {
-    //       console.log(res.error)
-    //       this.location.back()
-    //       const alert = await this.alertController.create({
-    //         header: 'Error',
-    //         message: res.error.message,
-    //         buttons: ['OK'],
-    //       });
-    //       await alert.present()
-    //     }
-    //   )
-
-    // REVERSE ARRAY IF READ MODE IS FROM BOTTOM TO TOP OR RIGHT TO LEFT
-    if (this.readMode === "bottomToTop" || this.readMode === "rightToLeft") {
-      this.images.reverse()
-    }
+    this.http.get(`https://sakura-mv.herokuapp.com/manga/${this.mangaID}/chapter/${this.chapterNumber}`)
+      .subscribe(
+        (res: any) => {
+          console.log(res)
+          this.images = res.chapter_images
+          // REVERSE ARRAY IF READ MODE IS FROM BOTTOM TO TOP OR RIGHT TO LEFT
+          if (this.readMode === "bottomToTop" || this.readMode === "rightToLeft") {
+            this.images.reverse()
+          }
+        },
+        async (res) => {
+          console.log(res.error)
+          this.location.back()
+          const alert = await this.alertController.create({
+            header: 'Error',
+            message: res.error.message,
+            buttons: ['OK'],
+          });
+          await alert.present()
+        }
+      )
   }
 
   ionViewDidEnter() {
