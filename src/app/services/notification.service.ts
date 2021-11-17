d439b164c63834c194bddca1f45f273f168302f9
JoseAIG <inciartegj@gmail.com>
2021-11-16 20:52:01 -0400

    Manga preview on notification tap
    Showing manga preview modal when entering the new chapter notification.

--- src/app/services/notification.service.ts
+++ src/app/services/notification.service.ts
@@ -2,6 +2,10 @@ import { Injectable } from '@angular/core';
 
 import { Capacitor } from '@capacitor/core';
 import { ActionPerformed, PushNotifications, PushNotificationSchema, Token } from '@capacitor/push-notifications';
+import { Manga } from '../interfaces/manga';
+import { MangaPreviewPage } from '../pages/manga-preview/manga-preview.page';
+import { ControllerService } from './controller.service';
+import { MangaService } from './manga.service';
 import { UserService } from './user.service';
 
 @Injectable({
@@ -9,7 +13,11 @@ import { UserService } from './user.service';
 })
 export class NotificationService {
 
-  constructor(private userService: UserService) { }
+  constructor(
+    private userService: UserService,
+    private controllerService: ControllerService,
+    private mangaService: MangaService
+  ) { }
 
   public initPush() {
     if (Capacitor.getPlatform() !== 'web') {
@@ -47,6 +55,27 @@ export class NotificationService {
 
     PushNotifications.addListener('pushNotificationActionPerformed', async (notification: ActionPerformed) => {
       console.log('Action performed:', JSON.stringify(notification.notification));
+      const mangaID: number = notification.notification.data.mangaID
+      //SHOW LOADING 
+      let loading = await this.controllerService.createLoading();
+      loading.present();
+      //FETCH MANGA
+      this.mangaService.getManga(mangaID)
+        .subscribe(
+          async (res: Manga) => {
+            loading.dismiss();
+            const modal = await this.controllerService.createModal({
+              component: MangaPreviewPage,
+              componentProps: {
+                manga: res
+              }
+            });
+            await modal.present();
+          },
+          async (res: any) => {
+            loading.dismiss();
+          }
+        )
     });
 
   }
